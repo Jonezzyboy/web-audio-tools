724a750873747506dc4a1d8aada309fee3b6ce5a
Alan Jones <alan.jones@chargehive.com>
2025-11-26 17:39:28 +0000

    Mmmmm web sockets

---
+++ server.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+console.log("Signaling server running on port 8080");
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    // Broadcast incoming message to all OTHER clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
